445fadb5d1480a16d02cf84e10433ac9481ca5e5
vladneyo <vlad27neychev95@gmail.com>
2016-04-05 21:42:13 +0300

    говно жопа

---
+++ Planner/Domain/Models/PublicationUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class DepartmentUser
+    {
+        public DepartmentUser()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        [Key]
+        public String Id { get; set; }
+        public String RateId { get; set; }
+        [ForeignKey("RateId")]
+        public virtual Rate Rate { get; set; }
+        public String DepartmentId { get; set; }
+        [ForeignKey("DepartmentId")]
+        public virtual Department Department { get; set; }
+
+    }
+}
