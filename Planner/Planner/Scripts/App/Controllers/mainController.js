74fdcd49f935e71b7c153235e0d6c9936cc24146
mozharovartem <mozharovartem@gmail.com>
2016-05-20 00:35:32 +0300

    Autocomplete

--- Planner/Planner/Scripts/App/Controllers/mainController.js
+++ Planner/Planner/Scripts/App/Controllers/mainController.js
@@ -1,5 +1,30 @@
-﻿PlannerApp.controller('mainController', ['$scope', function ($scope) {
+﻿PlannerApp.controller('mainController', ['$scope','$http', function ($scope,$http) {
+
     var me = this;
+    $scope.isHide = 0;
+    me.contributors = [];
+    me.teachersList = [];
+    me.init = function () {
+        $http.get('/Author/Get').then(
+            function (response) {
+                // на всякий случай проверь что вернулось в response.data
+                me.teachersList = response.data;
+            }, function (response) {
 
+            });
+    }
+    
+    me.addContributors = function (id) { //Сделал через id а не index, потому что index будет менятся по фильтру а в самом массиве index элемента останется прежним
+        for (var i = 0; i < me.teachersList.length; i++) {
+            if (me.teachersList[i].UserId === id) {
+                me.contributors.push(me.teachersList[i]);
+                me.teachersList.splice(i, 1);
+                break;
+            }
+        }
+    }
+    me.deleteFromContributors = function (index) {
+        me.teachersList.push(me.contributors[index]);
+        me.contributors.splice(index, 1);
+    }
 }]);
-
