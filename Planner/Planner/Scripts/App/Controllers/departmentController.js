a0529c424b321b8a93a56d11dd1895082fe1e965
vladislav.neychev <vladislav.neychev@chisw.com>
2016-06-24 15:10:53 +0300

    выровнял твое дерьмо

--- Planner/Planner/Scripts/App/Controllers/departmentController.js
+++ Planner/Planner/Scripts/App/Controllers/departmentController.js
@@ -5,7 +5,6 @@ PlannerApp.controller('departmentController', ['$scope', '$http', function ($sco
 
     $scope.departments = [];
     $scope.publications = [];
-    $scope.ScientificPublishing = {};
     $scope.years = [(new Date()).getFullYear() - 1, (new Date()).getFullYear(), (new Date()).getFullYear() + 1,
 					(new Date()).getFullYear() + 2, (new Date()).getFullYear() + 3];
     $scope.fromDate = null;
@@ -29,24 +28,6 @@ PlannerApp.controller('departmentController', ['$scope', '$http', function ($sco
             });
     };
 
-    $scope.$watch('departmentForPub', function (newVal) {
-        if (newVal) {
-            $http.get('/Department/DepartmentPublicationsReport?depId=' + newVal.Id).then(
-				function (response) {
-				    $scope.publications = response.data;
-				    console.log(response.data);
-				}, function (response) {
-
-				});
-            //$scope.departments = [].filter.call($scope.faculties, function (element, index, array) {
-            //	return element.Id == newVal;
-            //})[0].Departments;
-        }
-        else {
-            //$scope.departments = null;
-        }
-    });
-
     $scope.showHalfYear = function () {
         $http.get(`/Department/HalfYearDepartmentReport?depId=${$scope.departmentForHalfYear.Id}&year=${$scope.year}&half=${$scope.half}`).then(
 				function (response) {
@@ -60,26 +41,10 @@ PlannerApp.controller('departmentController', ['$scope', '$http', function ($sco
         var end = new Date($scope.toDate).toISOString();
         $http.get(`/Department/DateRangeDepartmentReport?depId=${$scope.departmentForPub.Id}&start=${start}&end=${end}`).then(
 				function (response) {
-				    $scope.ScientificPublishing = response.data;
+					$scope.publications = response.data;
 				}, function (response) {
 
 				});
     }
-    //$scope.$watch('departmentForHalfYear', function (newVal) {
-    //	if (newVal) {
-    //		$http.get('/Department/HalfYearDepartmentReport?depId=' + newVal.Id).then(
-    //			function (response) {
-    //				$scope.ScientificPublishing = response.data;
-    //				console.log($scope.ScientificPublishing);
-    //			}, function (response) {
 
-    //			});
-    //		//$scope.departments = [].filter.call($scope.faculties, function (element, index, array) {
-    //		//	return element.Id == newVal;
-    //		//})[0].Departments;
-    //	}
-    //	else {
-    //		//$scope.departments = null;
-    //	}
-    //});
 }]);
