654454bf0aea36bd33ea39a5fb59c3bc2eca6f6a
Alexander Anishchik <alexander.anishchik@itechcraft.com>
2016-06-24 14:16:24 +0300

    asd

--- Planner/Planner/Controllers/DepartmentController.cs
+++ Planner/Planner/Controllers/DepartmentController.cs
@@ -58,8 +58,14 @@ namespace Planner.Controllers
 			var model = PublicationReportBuilder.ScientificPublishing(depId, year, half);
 			return new JsonResult() { Data = model, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
 		}
-
-		public void PrintHalfYearDepartmentReport(string id, string name)
+        public JsonResult DateRangeDepartmentReport(string depId, DateTime start, DateTime end)
+        {
+          
+           //var model = PublicationReportBuilder.ScientificPublishing(depId, year, half);
+            //return new JsonResult() { Data = model, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            return new JsonResult() { Data = null, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        public void PrintHalfYearDepartmentReport(string id, string name)
 		{
 			//return File(filestream, "application/vnd.ms-excel", $"Публикации - {name} - {DateTime.Now.ToShortDateString()}.xls");
 		}
--- Planner/Planner/Scripts/App/Controllers/departmentController.js
+++ Planner/Planner/Scripts/App/Controllers/departmentController.js
@@ -1,65 +1,85 @@
 ﻿/// <reference path="../App.js" />
 /// <reference path="../../angular.js" />
 PlannerApp.controller('departmentController', ['$scope', '$http', function ($scope, $http) {
-	var me = this;
+    var me = this;
 
-	$scope.departments = [];
-	$scope.publications = [];
-	$scope.ScientificPublishing = {};
-	$scope.years = [(new Date()).getFullYear() - 1, (new Date()).getFullYear(), (new Date()).getFullYear() + 1,
+    $scope.departments = [];
+    $scope.publications = [];
+    $scope.ScientificPublishing = {};
+    $scope.years = [(new Date()).getFullYear() - 1, (new Date()).getFullYear(), (new Date()).getFullYear() + 1,
 					(new Date()).getFullYear() + 2, (new Date()).getFullYear() + 3];
+    $scope.fromDate = null;
+    $scope.toDate = null;
+    me.init = function () {
+        $('input[name="daterange"]').daterangepicker({
+            locale: {
+                format: 'YYYY-MM-DD'
+            }
+        },
+            function (start, end, label) {
+                $scope.fromDate = start;
+                $scope.toDate = end;
 
-	me.init = function () {
-	      $('input[name="daterange"]').daterangepicker();
-		$http.get('/Department/Get').then(
+            });
+        $http.get('/Department/Get').then(
             function (response) {
-            	$scope.departments = response.data;
+                $scope.departments = response.data;
             }, function (response) {
 
             });
-	};
+    };
 
-	$scope.$watch('departmentForPub', function (newVal) {
-		if (newVal) {
-			$http.get('/Department/DepartmentPublicationsReport?depId=' + newVal.Id).then(
+    $scope.$watch('departmentForPub', function (newVal) {
+        if (newVal) {
+            $http.get('/Department/DepartmentPublicationsReport?depId=' + newVal.Id).then(
 				function (response) {
 				    $scope.publications = response.data;
 				    console.log(response.data);
 				}, function (response) {
 
 				});
-			//$scope.departments = [].filter.call($scope.faculties, function (element, index, array) {
-			//	return element.Id == newVal;
-			//})[0].Departments;
-		}
-		else {
-			//$scope.departments = null;
-		}
-	});
+            //$scope.departments = [].filter.call($scope.faculties, function (element, index, array) {
+            //	return element.Id == newVal;
+            //})[0].Departments;
+        }
+        else {
+            //$scope.departments = null;
+        }
+    });
 
-	$scope.showHalfYear = function () {
-		$http.get(`/Department/HalfYearDepartmentReport?depId=${$scope.departmentForHalfYear.Id}&year=${$scope.year}&half=${$scope.half}`).then(
+    $scope.showHalfYear = function () {
+        $http.get(`/Department/HalfYearDepartmentReport?depId=${$scope.departmentForHalfYear.Id}&year=${$scope.year}&half=${$scope.half}`).then(
 				function (response) {
-					$scope.ScientificPublishing = response.data;
+				    $scope.ScientificPublishing = response.data;
 				}, function (response) {
 
 				});
-	}
-	//$scope.$watch('departmentForHalfYear', function (newVal) {
-	//	if (newVal) {
-	//		$http.get('/Department/HalfYearDepartmentReport?depId=' + newVal.Id).then(
-	//			function (response) {
-	//				$scope.ScientificPublishing = response.data;
-	//				console.log($scope.ScientificPublishing);
-	//			}, function (response) {
+    }
+    $scope.showByDateRange = function () {
+        var start = new Date($scope.fromDate).toISOString();
+        var end = new Date($scope.toDate).toISOString();
+        $http.get(`/Department/DateRangeDepartmentReport?depId=${$scope.departmentForPub.Id}&start=${start}&end=${end}`).then(
+				function (response) {
+				    $scope.ScientificPublishing = response.data;
+				}, function (response) {
 
-	//			});
-	//		//$scope.departments = [].filter.call($scope.faculties, function (element, index, array) {
-	//		//	return element.Id == newVal;
-	//		//})[0].Departments;
-	//	}
-	//	else {
-	//		//$scope.departments = null;
-	//	}
-	//});
+				});
+    }
+    //$scope.$watch('departmentForHalfYear', function (newVal) {
+    //	if (newVal) {
+    //		$http.get('/Department/HalfYearDepartmentReport?depId=' + newVal.Id).then(
+    //			function (response) {
+    //				$scope.ScientificPublishing = response.data;
+    //				console.log($scope.ScientificPublishing);
+    //			}, function (response) {
+
+    //			});
+    //		//$scope.departments = [].filter.call($scope.faculties, function (element, index, array) {
+    //		//	return element.Id == newVal;
+    //		//})[0].Departments;
+    //	}
+    //	else {
+    //		//$scope.departments = null;
+    //	}
+    //});
 }]);
