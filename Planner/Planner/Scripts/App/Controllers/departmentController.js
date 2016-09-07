a13d7b6cc6bea7edf7449547291aed3e15ae9f9e
Alexander Anishchik <alexander.anishchik@itechcraft.com>
2016-09-07 18:16:12 +0300

    f

--- Planner/Planner/Scripts/App/Controllers/departmentController.js
+++ Planner/Planner/Scripts/App/Controllers/departmentController.js
@@ -11,8 +11,38 @@ PlannerApp.controller('departmentController', ['$scope', '$http', function ($sco
     $scope.toDate = null;
     me.init = function () {
         $('input[name="daterange"]').daterangepicker({
-            locale: {
-                format: 'YYYY-MM-DD'
+            "locale": {
+                "format": 'YYYY-MM-DD',
+                "separator": " - ",
+                "applyLabel": "Прийняти",
+                "cancelLabel": "Вiдмiнити",
+                "fromLabel": "Вiд",
+                "toLabel": "До",
+                "customRangeLabel": "Custom",
+                "daysOfWeek": [
+                    "Пн",
+                    "Вт",
+                    "Ср",
+                    "Чт",
+                    "Пт",
+                    "Сб",
+                    "Вс"
+                ],
+                "monthNames": [
+                    "Січень",
+                    "Лютий",
+                    "Березень",
+                    "Квітень",
+                    "Травень",
+                    "Червень",
+                    "Липень",
+                    "Серпень",
+                    "Вересень",
+                    "Жовтень",
+                    "Листопад",
+                    "Грудень"
+                ],
+                "firstDay": 1
             }
         },
             function (start, end, label) {
@@ -28,34 +58,34 @@ PlannerApp.controller('departmentController', ['$scope', '$http', function ($sco
             });
     };
 
-	//department report for the date range
+    //department report for the date range
     $scope.showByDateRange = function () {
-    	var start = new Date($scope.fromDate).toISOString();
-    	var end = new Date($scope.toDate).toISOString();
-    	$http.get(`/Department/DateRangeDepartmentReport?depId=${$scope.departmentForPub.Id}&start=${start}&end=${end}`).then(
+        var start = new Date($scope.fromDate).toISOString();
+        var end = new Date($scope.toDate).toISOString();
+        $http.get(`/Department/DateRangeDepartmentReport?depId=${$scope.departmentForPub.Id}&start=${start}&end=${end}`).then(
 				function (response) {
-					$scope.publications = response.data;
+				    $scope.publications = response.data;
 				}, function (response) {
 
 				});
     };
-	//download department report for the date range
+    //download department report for the date range
     $scope.downloadDepartmentReport = function () {
-    	var start = new Date($scope.fromDate).toISOString();
-    	var end = new Date($scope.toDate).toISOString();
-    	window.location.assign(`/Department/PrintDepartmentPublicationsReport?depId=${$scope.departmentForPub.Id}&start=${start}&end=${end}`);
+        var start = new Date($scope.fromDate).toISOString();
+        var end = new Date($scope.toDate).toISOString();
+        window.location.assign(`/Department/PrintDepartmentPublicationsReport?depId=${$scope.departmentForPub.Id}&start=${start}&end=${end}`);
     };
 
-	//department report for the half of selected year
+    //department report for the half of selected year
     $scope.showHalfYear = function () {
-    	$http.get(`/Department/HalfYearDepartmentReport?depId=${$scope.departmentForHalfYear.Id}&year=${$scope.year}&half=${$scope.half}`).then(
+        $http.get(`/Department/HalfYearDepartmentReport?depId=${$scope.departmentForHalfYear.Id}&year=${$scope.year}&half=${$scope.half}`).then(
 				function (response) {
-					$scope.ScientificPublishing = response.data;
+				    $scope.ScientificPublishing = response.data;
 				}, function (response) {
 
 				});
     };
 
-    
+
 
 }]);
