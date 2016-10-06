1528c587d679bca84848c00afe97d7477b202aff
denkovalenko <deniskovalenko96@mail.ru>
2016-10-06 22:50:02 +0300

    -Add new Entities for PlannerDB -Add Migrations

---
+++ Planner/Domain/Models/PlanRemark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
+{
+    public class PlanRemark
+    {
+        public PlanRemark()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        [Key]
+        public String Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public string Remark { get; set; }
+
+        public string Signature { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+    }
+}
