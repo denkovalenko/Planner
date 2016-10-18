71a35467162d8b66565bcba41c83d17781747a46
AlexanderAnishchik <sanyaaxel94@gmail.com>
2016-10-18 23:45:37 +0300

    НДР

---
+++ Planner/Planner/Controllers/NdrController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Domain;
+using Domain.Models;
+using Planner.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web.Helpers;
+
+namespace Planner.Controllers
+{
+    [Authorize]
+    public class NdrController : Controller
+    {
+        public NdrController()
+        {
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Save(NdrViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["message"] = null;
+                return null;
+            }
+            Session["message"] = "Усi поля повиннi бути заповненi";
+            return RedirectToAction("Index");
+        }
+    }
+   
+}
