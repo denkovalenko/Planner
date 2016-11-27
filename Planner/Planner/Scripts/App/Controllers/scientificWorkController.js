4113e96723f1b07830f07eb6902b0a6845b73bc3
vladneyo <vlad27neychev95@gmail.com>
2016-11-27 22:13:54 +0200

    Scientific work. View prepared. Tabs and js object for fields mapping are made.

---
+++ Planner/Planner/Scripts/App/Controllers/scientificWorkController.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="../App.js" />
+/// <reference path="../../angular.js" />
+PlannerApp.controller('scientificWorkController', ['$scope', '$http', function ($scope, $http) {
+	var me = this;
+
+	$scope.value = "some text";
+}]);
